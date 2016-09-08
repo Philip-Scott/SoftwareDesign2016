e0f39125a40bc2094b37489fb2b4ca757082766c
philip-scott <felescoto95@hotmail.com>
2016-09-07 20:23:12 -0500

    Mockito & Maven test

---
+++ testingMavenAndMockito/src/main/java/mx/iteso/FooNotAvailable.java
@@ -0,0 +1,9 @@
+package mx.iteso;
+
+/**
+ * Created by felipe on 9/5/16.
+ */
+public class FooNotAvailable extends RuntimeException {
+
+}
+
---
+++ testingMavenAndMockito/src/main/java/mx/iteso/InvalidQuestion.java
@@ -0,0 +1,7 @@
+package mx.iteso;
+
+/**
+ * Created by felipe on 9/5/16.
+ */
+public class InvalidQuestion extends RuntimeException {
+}
