2eb18e248f202636d85264411583776cc54c5aa0
philip-scott <felescoto95@hotmail.com>
2016-09-29 07:16:46 -0500

    Add size constants

--- decoratorPattern/src/main/java/mx/iteso/decorator/Taco.java
+++ decoratorPattern/src/main/java/mx/iteso/decorator/Taco.java
@@ -1,6 +1,12 @@
 package mx.iteso.decorator;
 
 public abstract class Taco {
+    public static final String MEGA = "MEGA";
+    public static final String REGULAR = "REGULAR";
+    public static final String MINI = "MINI";
+
+
+    public String size = REGULAR;
     public String description = "Any Taco";
     public String getDescription(){
         return description;
