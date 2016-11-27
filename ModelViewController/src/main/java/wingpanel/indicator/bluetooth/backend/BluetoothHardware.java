6fbec9f153955b70f72efdf3887936ebcf45deb5
philip-scott <felescoto95@hotmail.com>
2016-11-26 18:35:40 -0600

    Merged Diego's unit test

--- ModelViewController/src/main/java/wingpanel/indicator/bluetooth/backend/BluetoothHardware.java
+++ ModelViewController/src/main/java/wingpanel/indicator/bluetooth/backend/BluetoothHardware.java
@@ -4,7 +4,13 @@ package wingpanel.indicator.bluetooth.backend;
  * Created by felipe on 11/26/16.
  */
 public class BluetoothHardware {
+    private static final String defaultState = "Userslaptop false speakers";
+
     // Mocks a bluetooth interface
     // <display name> <State> <paired device 1> <paired device 2> ...
-    public static String state = "Userslaptop disabled speakers";
+    public static String state = defaultState;
+
+    public static void reset () {
+        state = defaultState;
+    }
 }
